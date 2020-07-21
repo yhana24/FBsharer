b8a6f1c3ff1c3689813f6c86b8b10eb29b0a87d7
Jonhson1997 <johnson97312@gmail.com>
2020-07-21 10:45:00 +0800

    20200721

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>FB Sharer</title>
+    <!-- FB Sharer -->
+    <meta name="og:url" content="YOUR WEBSITE" />
+    <meta name="og:type" content="website" />
+    <meta name="og:title" content="OMG" />
+    <meta name="og:description" content="OMGOMG" />
+    <meta property="og:image" content="YOUR PICTURE" />
+    <!-- ↓JPG↓ -->
+    <meta property="og:image:type" content="image/jpeg" />
+    <!-- ↓PNG↓ -->
+    <!-- <meta property="og:image:type" content="image/png" /> -->
+    <meta property="og:image:width" content="960" />
+    <meta property="og:image:height" content="822" />
+    <!-- FB Sharer End -->
+</head>
+<body>
+    <!-- ↓Share URL↓ -->
+    <a href="https://www.facebook.com/sharer/sharer.php?u=https://www.jddt.tw/" class="" target="_blank">
+        <img src="img_fbShare.png" alt="" />
+    </a>
+</body>
+</html>
