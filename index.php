d9e95c1a8846009639baa30dd1d80f8336cb04bf
yhana24 <armiayasis24@gmail.com>
2024-12-31 01:59:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>FB Sharer</title>
+    <title>Facebook Sharer</title>
     <!-- FB Sharer -->
     <meta name="og:url" content="YOUR WEBSITE" />
     <meta name="og:type" content="website" />
